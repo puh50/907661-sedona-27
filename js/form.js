93e9ecf654fba9105108e5f4189f087cc43b821f
Andrei Gaponenko <puhpuh690@gmail.com>
2020-02-25 16:46:40 +0300

    Добавлена анимация формы и интерактивная карта

---
+++ js/form.js
@@ -0,0 +1,26 @@
+var searchButton = document.querySelector('.hotel-search-button');
+
+var form = document.querySelector(".hotel-search-form");
+var dateFrom = form.querySelector(".from-date");
+var dateTo = form.querySelector(".to-date");
+var parentNumber = form.querySelector(".parent-number");
+var childrenNumber = form.querySelector(".children-number");
+var errMessage = form.querySelector(".error-message");
+
+searchButton.addEventListener("click", function () {
+    form.classList.toggle("form-hide");
+
+    if (dateFrom.value && dateTo.value && parentNumber.value && childrenNumber.value) {
+        errMessage.classList.remove("error-message-show");
+    };
+});
+
+form.addEventListener("submit", function (evt) {
+    if (!dateFrom.value || !dateTo.value || !parentNumber.value || !childrenNumber.value) {
+        evt.preventDefault();
+        form.classList.remove("form-error");
+        form.offsetWidth = form.offsetWidth;
+        form.classList.add("form-error");
+        errMessage.classList.add("error-message-show");
+    }
+});
